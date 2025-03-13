a00d9f836383d0fcd7636dad862a0f20a89a0339
GnuTVan <dangtung011202@gmail.com>
2025-03-13 23:17:32 +0700

    handle source BE

--- src/main/java/com/inventorymanagement/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.inventorymanagement.repository;
-
-import com.inventorymanagement.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-    boolean existsByCode(String code);
-    Category findByCode(String code);
-}
