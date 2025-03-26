60f1ca884e96a41a5fc2267372e12f0f14499acb
GnuTVan <dangtung011202@gmail.com>
2025-03-26 16:01:32 +0700

    fix order services

--- src/main/java/com/inventorymanagement/services/IOrderServices.java
+++ src/main/java/com/inventorymanagement/services/IOrderServices.java
@@ -10,106 +10,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Service
-@ReRequiredArgsConstructor
-public class OrderServicesImpl implements IOrderServices {
+
+public interface IOrderServices {
     void createOrder(OrderCreateDTO dto, String authHeader) throws InventoryException;
-     @Transactional
-    @Override
-    public void updateOrder(OrderCreateDTO dto, String authHeader, String code) throws InventoryException {
-        Employee me = employeeService.getFullInformation(authHeader);
-        if(Objects.isNull(me) || me.getRoleCode().contains(RoleEnum.SALE.name())){
-            throw new InventoryException(
-                    ExceptionMessage.NO_PERMISSION,
-                    ExceptionMessage.messages.get(ExceptionMessage.NO_PERMISSION)
-            );
-        }
-        Optional<Order> orderOp = orderRepository.findByCode(code);
-        if(orderOp.isEmpty()){
-            throw new InventoryException(
-                    ExceptionMessage.ORDER_NOT_EXISTED,
-                    ExceptionMessage.messages.get(ExceptionMessage.ORDER_NOT_EXISTED)
-            );
-        }
-        Order order = orderOp.get();
-        List<String> approveStatus = List.of(PURCHASE_ORDER_APPROVE.REJECTED.name(), PURCHASE_ORDER_APPROVE.APPROVED.name());
-        if(approveStatus.contains(order.getApproveStatus())){
-            throw new InventoryException(
-                    ExceptionMessage.ORDER_APPROVED,
-                    ExceptionMessage.messages.get(ExceptionMessage.ORDER_APPROVED)
-            );
-        }
-        order.setTotalAmount(dto.getTotalAmount());
-        orderRepository.save(order);
-        orderProductRepository.deleteByOrderCode(code);
-        List<OrderProduct> orderProducts = new ArrayList<>();
-        for (ProductOrderCreateDTO item : dto.getProducts()) {
-            OrderProduct orderProduct = OrderProduct.builder()
-                    .orderCode(orderOp.get().getCode())
-                    .productCode(item.getProductCode())
-                    .quantity(item.getQuantity())
-                    .discount(item.getDiscount())
-                    .build();
-            orderProducts.add(orderProduct);
-        }
-        orderProductRepository.saveAll(orderProducts);
-    }
-    @Override
-    public void approveOrder(String orderCode, String authHeader) throws InventoryException {
-        Employee me = employeeService.getFullInformation(authHeader);
-        if(me == null || me.getRoleCode().contains(RoleEnum.SALE.name())){
-            throw new InventoryException(
-                    ExceptionMessage.NO_PERMISSION,
-                    ExceptionMessage.messages.get(ExceptionMessage.NO_PERMISSION)
-            );
-        }
-        Optional<Order> orderOp = orderRepository.findByCode(orderCode);
-        if(orderOp.isEmpty()){
-            throw new InventoryException(
-                    ExceptionMessage.ORDER_NOT_EXISTED,
-                    ExceptionMessage.messages.get(ExceptionMessage.ORDER_NOT_EXISTED)
-            );
-        }
-        Order order = orderOp.get();
-        if(order.getApproveStatus().equals(PURCHASE_ORDER_APPROVE.REJECTED.name())){
-            throw new InventoryException(
-                    ExceptionMessage.ORDER_REJECT,
-                    ExceptionMessage.messages.get(ExceptionMessage.ORDER_REJECT)
-            );
-        }
-        order.setApproveBy(me.getName());
-        order.setApproveStatus(PURCHASE_ORDER_APPROVE.APPROVED.name());
-        order.setApproveDate(LocalDateTime.now());
-        orderRepository.save(order);
-    } throws InventoryException;
+    @Transactional
+    void updateOrder(OrderCreateDTO dto, String authHeader, String code) throws InventoryException;
+    void approveOrder(String orderCode, String authHeader) throws InventoryException;
     OrderDTO findOrderByCode(String orderCode) throws InventoryException;
-    @Override
-    public void rejectOrder(String orderCode, String authHeader) throws InventoryException {
-        Employee me = employeeService.getFullInformation(authHeader);
-        if(me == null || me.getRoleCode().contains(RoleEnum.SALE.name())){
-            throw new InventoryException(
-                    ExceptionMessage.NO_PERMISSION,
-                    ExceptionMessage.messages.get(ExceptionMessage.NO_PERMISSION)
-            );
-        }
-        Optional<Order> orderOp = orderRepository.findByCode(orderCode);
-        if(orderOp.isEmpty()){
-            throw new InventoryException(
-                    ExceptionMessage.ORDER_NOT_EXISTED,
-                    ExceptionMessage.messages.get(ExceptionMessage.ORDER_NOT_EXISTED)
-            );
-        }
-        Order order = orderOp.get();
-        if(order.getApproveStatus().equals(PURCHASE_ORDER_APPROVE.APPROVED.name())){
-            throw new InventoryException(
-                    ExceptionMessage.ORDER_APPROVED,
-                    ExceptionMessage.messages.get(ExceptionMessage.ORDER_APPROVED)
-            );
-        }
-        order.setApproveBy(me.getName());
-        order.setApproveStatus(PURCHASE_ORDER_APPROVE.REJECTED.name());
-        order.setApproveDate(LocalDateTime.now());
-        orderRepository.save(order);
-    }
+    void rejectOrder(String orderCode, String authHeader) throws InventoryException;
     Page<OrderDTO> findBySearchRequest(OrderSearchReqDTO reqDTO, Pageable pageable);
 }
